a0a527c195ce0a0a489a8b83190e7869ed22aaf9
Darwin Morocho <darwin@MacBook-Pro-de-Darwin.local>
2020-05-06 18:31:17 -0500

    updated ios example to login with twitter

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,6 +1,8 @@
 import UIKit
 import Flutter
-import FBSDKCoreKit // <--- ADD THIS LINE
+import FBSDKCoreKit // <--- ADD THIS LINE (first you need run  pod install)
+import TwitterKit // <--- FOR LOGIN WITH TWITTER ADD THIS LINE (first you need run  pod install)
+
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,7 +10,7 @@ import FBSDKCoreKit // <--- ADD THIS LINE
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
     ) -> Bool {
-        //ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)// <--- ADD THIS LINE
+        ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)// <--- ADD THIS LINE
         GeneratedPluginRegistrant.register(with: self)
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
@@ -16,11 +18,19 @@ import FBSDKCoreKit // <--- ADD THIS LINE
 
     // <--- OVERRIDE THIS METHOD WITH THIS CODE
     override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        if let facebookAppId: String = Bundle.main.object(forInfoDictionaryKey: "FacebookAppID") as? String {
-            if url.scheme != nil && url.scheme!.hasPrefix("fb\(facebookAppId)") && url.host ==  "authorize" {
-                return ApplicationDelegate.shared.application(app, open: url, options: options)
-           }
+       
+        if url.scheme != nil {
+            let facebookAppId: String? = Bundle.main.object(forInfoDictionaryKey: "FacebookAppID") as? String
+            if facebookAppId != nil && url.scheme!.hasPrefix("fb\(facebookAppId!)") && url.host ==  "authorize" {
+                 print("is login by facebook")
+                 return ApplicationDelegate.shared.application(app, open: url, options: options)
+            } else if url.scheme!.contains("twitter") { // for login by twitter
+               print("is login by twitter")
+               return TWTRTwitter.sharedInstance().application(app, open: url, options: options)
+            }
         }
+        
         return false
+
     }
 }
